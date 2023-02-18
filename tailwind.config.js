a8cf6485dc22e5d19f0813d6d5dbdeaa80088796
Swati <swatic019@gmail.com>
2023-02-18 18:39:51 +0530

    header created

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html',],
+  content: ["./index.html"],
   theme: {
     extend: {},
+    colors: {
+      blue: "#2874f0",
+      yellow:'#ffe500',
+      white: 'rgb(255 255 255);',
+    },
+    fontFamily: {
+      roboto:'Roboto,Arial,sans-serif;'
+    }
   },
   plugins: [],
-}
+};
