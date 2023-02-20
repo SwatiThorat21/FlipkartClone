275ec0f00e125e9bce4604b86cdd1872753b903b
Swati <swatic019@gmail.com>
2023-02-20 12:23:05 +0530

    header done

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,18 @@ module.exports = {
     extend: {},
     colors: {
       blue: "#2874f0",
-      yellow:'#ffe500',
-      white: 'rgb(255 255 255);',
+      yellow: "#ffe500",
+      white: "rgb(255 255 255);",
     },
     fontFamily: {
-      roboto:'Roboto,Arial,sans-serif;'
-    }
+      roboto: "Roboto,Arial,sans-serif;",
+    },
+    fontSize: {
+      belowBase: "0.95rem",
+      xs: "0.75rem",
+      sm: "0.875rem",
+      lg: "1.125rem",
+    },
   },
   plugins: [],
 };
