24a0d3f4f04ba95f2394258afe4107c9a5ec6f89
Swati <swatic019@gmail.com>
2023-02-24 16:42:39 +0530

    image slider updated

--- code.js
+++ code.js
@@ -8,23 +8,38 @@ function renderProducts() {
     .then((data) => {
       console.log(data);
       let featureProductList = document.getElementById("featureProduct_list");
+      let SlidingImgContainer = document.getElementById("imageContainer");
       let featuredProductsHTML = "";
+      let slidingImgHTML = "";
 
       data.featuredProducts.forEach((product) => {
         featuredProductsHTML += `
-        <div id="featuredProduct_item" class="py-3 px-1">
-        <div id="featuredProduct_image" class="w-16">
+        <div id="featuredProduct_item" class="py-3 px-1 flex flex-col items-center">
+        <div id="featuredProduct_image" class="w-w16">
             <img src="${product.img}"/>
         </div>
         <p class="text-sm font-semibold text-gray">
             ${product.name}
-            ${product.subNavigation ? `<i class="fa-solid fa-angle-down text-xs"></i>` : ""}   
+            ${
+              product.subNavigation
+                ? `<i class="fa-solid fa-angle-down text-xs"></i>`
+                : ""
+            }   
         </p>
         </div>
-
                        `;
       });
-      featureProductList.insertAdjacentHTML("beforeend",featuredProductsHTML);
+      featureProductList.insertAdjacentHTML("beforeend", featuredProductsHTML);
+
+      data.slidingImages.forEach((img) => {
+        slidingImgHTML += `
+        <div class="w-full">
+            <img src="${img.imgSrc}"
+              alt="" class="w-full">
+        </div>
+        `;
+      });
+      SlidingImgContainer.insertAdjacentHTML("beforeend", slidingImgHTML);
     });
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html","./code.js"],
+  content: ["./index.html", "./code.js"],
   theme: {
     extend: {},
     colors: {
@@ -8,6 +8,7 @@ module.exports = {
       yellow: "#ffe500",
       white: "rgb(255 255 255);",
       gray: "#474545",
+      sliderBgColor: "#f1f3f6",
     },
     fontFamily: {
       roboto: "Roboto,Arial,sans-serif;",
@@ -17,11 +18,29 @@ module.exports = {
       xs: "0.75rem",
       sm: "0.875rem",
       lg: "1.125rem",
+      threexl: "2rem",
     },
     boxShadow: {
-      gray: "0 1px 1px 0 rgb(0 0 0 / 16%)",
+      featureGray: "0 1px 1px 0 rgb(0 0 0 / 16%)",
       btnShadow: "1px 1px 5px rgba(0,0,0,0.1)",
     },
+    maxWidth: {
+      maxSliderImg: "1680px",
+      maxFeatureList: "1280px",
+      maxSearchBar: "564px"
+    },
+    minWidth: {
+      minSliderImg: "978px",
+    },
+    width: {
+      full: "100%",
+      w20: "80px",
+      w3: "12px",
+      w16: "64px",
+      wSlidingImg: "99.1%",
+      featureList: "1280px"
+
+    }
   },
   plugins: [],
 };
